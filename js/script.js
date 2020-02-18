479ed4a502c367fd8a4a94c5f6dba794559350ad
Luis Giraldo <lugirva@gmail.com>
2020-02-18 15:34:01 -0500

    Added code to meet expecrtations
    Added appendPageLinks function
    Added extra logic to handle element creation and event handler

--- js/script.js
+++ js/script.js
@@ -4,72 +4,134 @@ FSJS project 2 - List Filter and Pagination
 ******************************************/
    
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
+/*** 
+ * IIFE to not clutter the Global Object
+***/
 (function(){
    /*** 
-      Add your global variables that store the DOM elements you will 
-      need to reference and/or manipulate. 
-      
-      But be mindful of which variables should be global and which 
-      should be locally scoped to one of the two main functions you're 
-      going to create. A good general rule of thumb is if the variable 
-      will only be used inside of a function, then it can be locally 
-      scoped to that function.
+    * Global variables
    ***/
-   const student_list = document.querySelectorAll('.student-item');
+   const student_list = document.querySelectorAll(".student-item");
    const items_per_page = 10;
 
-
-
-   /*** 
-      Create the `showPage` function to hide all of the items in the 
-      list except for the ten you want to show.
-
-      Pro Tips: 
-      - Keep in mind that with a list of 54 students, the last page 
-         will only display four.
-      - Remember that the first student has an index of 0.
-      - Remember that a function `parameter` goes in the parens when 
-         you initially define the function, and it acts as a variable 
-         or a placeholder to represent the actual function `argument` 
-         that will be passed into the parens later when you call or 
-         "invoke" the function 
+   /***
+    * `showPage` function
+    * @param {NodeList} list - holds the list of all students
+    * @param {Number} page - holds the index of the current page
+    * Display (items_per_page) number of students on the page
    ***/
    function showPage(list, page){
       const start_index = (page * items_per_page) - items_per_page;
       const end_index = page * items_per_page;
-      console.log("list length: " + list.length);
 
       for (let i = 0, len = list.length; i < len ; i++){
          if(i >= start_index && i < end_index){
-            list[i].style.display = '';
+            list[i].style.display = "";
          } else {
-            list[i].style.display = 'none';
+            list[i].style.display = "none";
          }
       } // endFor
    }
 
-   showPage(student_list, 3);
-
-   function createElem(name, class_name){
+   /***
+    * `createElem` function
+    * returns a new element
+    * @param {String} name - holds the tag name of the element to be created
+    * @param {Object} elemAttributes - holds an object that contains all attributes that are going to be added to the element
+    * Creates an element based on the values that are passed in
+   ***/
+   function createElem(name, elemAttributes){
       const element = document.createElement(name);
-      if(class_name){
-         element.className = class_name;
+      if(elemAttributes){
+         if(elemAttributes.elClass){
+            element.classList = elemAttributes.elClass;
+         }
+
+         if(elemAttributes.href){
+            element.href = elemAttributes.href;
+         }
+
+         if(elemAttributes.text){
+            element.textContent = elemAttributes.text;
+         }
+
+         if(elemAttributes.data){
+            element.dataset.name = elemAttributes.data;
+         }
       }
       return element;
    }
-   /*** 
-      Create the `appendPageLinks function` to generate, append, and add 
-      functionality to the pagination buttons.
-   ***/
 
-   function appendPageLinks(list){
-      const div = createElem('DIV', "pagination");
-      const ul = createElem('UL');
-      console.log(div);
-      console.log(ul);
+   /***
+    * `paginationCallback` function
+    * @param {Object} event - holds the value of the event object
+    * removes the active class from all the anchor tags.
+    * later adds the active class to the element who triggered the event.
+    * calls the showPage function to display students based on the page selected
+   ***/
+   function paginationCallback(e){
+      e.preventDefault();
+      if(e.target.dataset["name"] === "anchor"){
+         const anchors = document.querySelectorAll("[data-name]");
+         for(let i = 0, len = anchors.length; i < len; i++){
+            anchors[i].classList.remove("active");
+         }
+
+         e.target.classList.add("active");
+         showPage(student_list, e.target.textContent);
+      }
    }
 
+   /***
+    * `createAnchors` function
+    * @param {Number} i - holds the actual value of the counter for the loop, whom called this function 
+    * Creates an anchor elements an adds attributes based on the number of the page(i)
+   ***/
+   function createAnchors(i){
+      let a;
+      if(i === 0){
+         a = createElem("a", {
+            elClass: "active",
+            href: "#",
+            text: i + 1,
+            data: "anchor"
+         });
+      } else {
+         a = createElem("a", {
+            href: "#",
+            text: i + 1,
+            data: "anchor"
+         });
+      }
+
+      return a;
+   }
+
+   /***
+    * `appendPageLinks` function
+    * @param {NodeList} list - holds the list of all students
+    * Calls createElem function and with the elements returned, appends it to the page
+   ***/
+   function appendPageLinks(list){
+      const divPage = document.querySelector(".page");
+      const divPagination = createElem("DIV", { 
+                                       elClass: "pagination"
+                                    });
+                             
+      const ul = createElem("UL");
+      ul.addEventListener("click", paginationCallback);
+
+      for (let i = 0, len = list.length/items_per_page; i < len; i++){
+         let li = createElem("LI");
+         let a = createAnchors(i);
+         li.appendChild(a);
+         ul.appendChild(li);
+      }
+      divPagination.appendChild(ul);
+      divPage.appendChild(divPagination);
+   }
+
+   showPage(student_list, 1);
    appendPageLinks(student_list);
   // pagination HTML to create dynamically 
 //   <div class="pagination">
